9aba693f1058d2f2afab26a266c5f8226ad76364
rahra <bf@abenteuerland.at>
2019-01-29 21:17:51 +0100

    added default IPv4 gateway to rtt script

--- rrdscripts/rtt.sh
+++ rrdscripts/rtt.sh
@@ -2,6 +2,21 @@
 
 iplist="www.google.com"
 
+which netstat >/dev/null
+res_netstat=$?
+which ip >/dev/null
+res_ip=$?
+
+gw4=
+if test $res_netstat -eq 0 ; then
+   gw4=$(netstat -nr4 | grep -E "^default|^0.0.0.0" | tr -s \  | cut -d \  -f 2)
+#   gw6=$( netstat -nr6 | grep -E "^default|^::/0" | grep UG  | grep -v fe80 | tr -s \  | cut -d \  -f 2)
+elif test $res_ip -eq 0 ; then
+   gw4=$(ip route | grep -E ^default | cut -d \  -f 3)
+fi
+
+iplist="$gw4 $iplist"
+
 RRDDATA=
 
 for ip in $iplist
